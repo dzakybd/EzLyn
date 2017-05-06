26b43d942e9879816b26d5d67d9b7a5c4319c9d8
dzakybd <dzakybd@gmail.com>
2017-05-07 00:57:35 +0700

    menambahkan sharedpreference form data angkot + menambahkan fitur merubah data angkot

--- driverezlyn/src/main/java/id/ac/its/driverezlyn/activity/RegistrationActivity.java
+++ driverezlyn/src/main/java/id/ac/its/driverezlyn/activity/RegistrationActivity.java
@@ -1,50 +1,116 @@
 package id.ac.its.driverezlyn.activity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.pixplicity.easyprefs.library.Prefs;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import id.ac.its.driverezlyn.R;
+import id.ac.its.driverezlyn.model.Lyn;
 
 public class RegistrationActivity extends AppCompatActivity {
-    public EditText in_plat;
-    public Spinner in_kode;
-    public String no_plat, kode;
-    private Button btn;
 
-    String[] kode_lyn = {"S","WK","O"};
+    @BindView(R.id.toolbar_title)
+    TextView toolbarTitle;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.plat_nomor)
+    EditText platNomor;
+    @BindView(R.id.harga)
+    EditText harga;
+    @BindView(R.id.next_regis)
+    Button nextRegis;
+    DatabaseReference databaseLyn;
+    Lyn lyn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        ButterKnife.bind(this);
+        if (!Prefs.getString("plat", "").isEmpty()) {
+            move();
+        }
+        databaseLyn = FirebaseDatabase.getInstance().getReference("lyn");
+        platNomor.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
 
-        Spinner mySpinner = (Spinner)findViewById(R.id.kodeAngkot);
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                ifedit();
+            }
+        });
 
-        ArrayAdapter<String> myAdapt = new ArrayAdapter<String> (this ,
-                android.R.layout.simple_list_item_1,
-                getResources().getStringArray(R.array.list_kode));
-        myAdapt.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mySpinner.setAdapter(myAdapt);
+        harga.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
 
-        in_plat = (EditText) findViewById(R.id.plat_nomor);
-        in_kode = (Spinner) findViewById(R.id.kodeAngkot);
-        btn = (Button)findViewById(R.id.next_regis);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                ifedit();
+            }
+        });
     }
 
-    public void goRegis(View v){
-        no_plat = in_plat.getText().toString().trim();
-        kode = in_kode.getSelectedItem().toString();
+    private void ifedit(){
+        if(!harga.getText().toString().isEmpty()&&!platNomor.getText().toString().isEmpty()){
+            nextRegis.setVisibility(View.VISIBLE);
+        }else nextRegis.setVisibility(View.GONE);
+    }
 
-        Intent i = new Intent(getApplicationContext(), validasi.class);
-        i.putExtra("lyn_code", kode);
-        i.putExtra("plate_code", no_plat);
+    @OnClick(R.id.next_regis)
+    public void onViewClicked() {
+        String a=platNomor.getText().toString();
+        int b=Integer.parseInt(harga.getText().toString());
+        lyn = new Lyn(a,b,false,true, -7.275622, 112.793449);
+        databaseLyn.child(lyn.getPlate()).setValue(lyn);
+        Prefs.putString("plat",a);
+        Prefs.putInt("harga",b);
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Menyimpan");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.setCancelable(false);
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.show();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                progressDialog.dismiss();
+                move();
+            }
+        }, 2000);
+    }
+
+    private void move(){
+        Intent i = new Intent(this, LynStopActivity.class);
         startActivity(i);
         finish();
     }
+
 }
